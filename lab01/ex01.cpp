e27d42473761252c25718250d143fa97d0693474
Fabricio Rodrigues <76504799+fab-rodrigs@users.noreply.github.com>
2024-10-10 14:04:35 -0300

    Update ex01.cpp

--- lab01/ex01.cpp
+++ lab01/ex01.cpp
@@ -4,12 +4,6 @@ Laborátorio 01: Implementando classes (abstrações)
 Aluno: Fabrício Rodrigues de Santana
 
 Exercício 01
-
-Crie uma classe Pessoa para representar uma pessoa, com os atributos privados de nome,
-idade e altura. Crie os métodos públicos necessários para acessar e modificar os atributos
-desta classe e crie um método para imprimir os dados da pessoa.
-
-Implemente um programa de teste que utilize toda a interface da classe criada.
 */
 
 #include <iostream>
