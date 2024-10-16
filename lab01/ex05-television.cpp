6bb7cf37eb021e5bf8c25788529426726ae83ce3
Fabricio Rodrigues <76504799+fab-rodrigs@users.noreply.github.com>
2024-10-15 23:25:07 -0300

    Create ex05-television.cpp

---
+++ lab01/ex05-television.cpp
@@ -0,0 +1,61 @@
+#include "Television.h"
+#include <iostream>
+
+using namespace std;
+
+Television::Television()
+{
+	_volume = 0;
+	_channel = 0;
+}
+
+void Television::increaseVolume()
+{
+	if (_volume < 100) {
+		_volume++;
+	}
+	else {
+		cout << "\nMax volume!" << endl;
+	}
+}
+
+void Television::decreaseVolume()
+{
+	if (_volume > 0) {
+		_volume--;
+	}
+	else {
+		cout << "\nMin volume!" << endl;
+	}
+}
+
+void Television::increaseChannel()
+{
+	if (_channel < 75) {
+		_channel++;
+	}
+	else {
+		cout << "\nMax channel!" << endl;
+	}
+}
+
+void Television::decreaseChannel()
+{
+	if (_channel > 0) {
+		_channel--;
+	}
+	else {
+		cout << "\nMin channel!" << endl;
+	}
+}
+
+void Television::getVolume()
+{
+	cout << "\nVolume: " << _volume << endl;
+}
+
+void Television::getChannel()
+{
+	cout << "\nChannel: " << _channel << endl;
+}
+
